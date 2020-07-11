b577a88edc2aa97ad6e4a7a537c2680ecfd7174b
nick <njd5475@gmail.com>
2020-07-11 11:43:45 -0400

    Update gems.

--- app/assets/javascripts/components/success_message.jsx
+++ app/assets/javascripts/components/success_message.jsx
@@ -4,8 +4,10 @@ class SuccessMessage extends React.Component {
   render() {
     return (
         <h1>  
+          <div className="alert alert-success">We have alerts!</div>
           Success
           <small> Made with an automated build deploy process.</small>
+          <button>Press Me</button>
         </h1>
     );
   }
